60279dd0ead49ef76f9c0d92005b513b310b7ee6
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-22 21:48:25 +0200

    fix: rename attributes fusion latlong in aray

--- trombi/src/Component/MapView.jsx
+++ trombi/src/Component/MapView.jsx
@@ -3,24 +3,21 @@ import { StudientData } from "./StudientData";
 import { useState } from "react";
 
 const MapView = () => {
-  const [position, setPostion] = useState([lat, long]);
-  const ZOOM_LEVEL = 2;
-
-  const lat = StudientData.map((item, index) => console.log(item.lat));
-  const long = StudientData.map((item, index) => console.log(item.long));
+  const position = [51.505, -0.09];
+  const ZOOM_LEVEL = 12;
 
   return (
     <div className="container">
       <MapContainer scrollWheelZoom={false} center={position} zoom={ZOOM_LEVEL}>
-        {StudientData.map((item) => (
-          <Marker position={setPostion(item)}>
-            <Popup>{item.nom}</Popup>
-          </Marker>
-        ))}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {StudientData.map((studient, index) => (
+          <Marker key={index} position={studient.latlong}>
+            <Popup></Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );

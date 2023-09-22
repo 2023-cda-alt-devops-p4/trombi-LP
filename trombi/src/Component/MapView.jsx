4d7aab84f6452feb68bbde94b16ed972041af371
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-22 21:04:40 +0200

    ci: test display

--- trombi/src/Component/MapView.jsx
+++ trombi/src/Component/MapView.jsx
@@ -1,22 +1,28 @@
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import { StudientData } from "./StudientData";
+import { useState } from "react";
 
 const MapView = () => {
-    const position = [49.847503, 2.763062]
-    const ZOOM_LEVEL = 2;
+  const [position, setPostion] = useState([lat, long]);
+  const ZOOM_LEVEL = 2;
 
-    return (
-        <div className="container">
-            <MapContainer
-                scrollWheelZoom={false}
-                center={position}
-                zoom={ZOOM_LEVEL}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
-        </div>
-    )
-}
-export default MapView
+  const lat = StudientData.map((item, index) => console.log(item.lat));
+  const long = StudientData.map((item, index) => console.log(item.long));
+
+  return (
+    <div className="container">
+      <MapContainer scrollWheelZoom={false} center={position} zoom={ZOOM_LEVEL}>
+        {StudientData.map((item) => (
+          <Marker position={setPostion(item)}>
+            <Popup>{item.nom}</Popup>
+          </Marker>
+        ))}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+export default MapView;

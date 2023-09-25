dbfb70d01333a2fd89076a573296f67648c2eb02
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-09-25 10:27:55 +0200

    fix: display footer

--- trombi/src/App.jsx
+++ trombi/src/App.jsx
@@ -6,13 +6,13 @@ import { StudientData } from "./Component/StudientData";
 function App() {
   return (
     <div className="App">
-      {/* <MapView
+      <MapView
         withHeight="100vh"
         zoom={8.5}
         center={[49.63297, 3.05858]}
         markers={StudientData}
         scrollWheelZoom={true}
-      /> */}
+      />
       <Footer />
     </div>
   );

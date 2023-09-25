10789369b22b05d900b4b3a59781f3d415391e25
Deflandrenassim <nassim.def@gmail.com>
2023-09-25 10:25:30 +0200

    feat : design header

---
+++ trombi/src/Component/Header.jsx
@@ -0,0 +1,9 @@
+import 'animate.css';
+
+export const Header = () => {
+    return (
+        <header className="head">
+            <h1 class="animate__animated animate__backInRight">Présentation du trombi de Ludovic & Nassim </h1>
+        </header>
+    )
+}
